d053c94421b2e13e7cd131c286fb3b76801a5909
KaijuML <clement.rebuffel@lip6.fr>
2021-05-23 15:16:48 +0200

    loading data and models in run

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from argparse import ArgumentParser
 
+from models import ConvRgModel, RecurrentRgModel
+from sampler import build_dataset_iter
+from data import prep_data
+from trainer import Trainer
+
 
 def get_parser():
     parser = ArgumentParser(description='Train/Use an Information Extractor '
@@ -18,7 +23,7 @@ def get_parser():
                        help='path to hdf5 file containing candidate relations '
                             'from generated data')
     group.add_argument('--savefile', dest='savefile', default='',
-                       help='path to save model to')
+                       help='path to a directory where to model should be saved')
     group.add_argument('--eval-models', dest='eval_models', default='',
                        help='path to trained extractor models')
     group.add_argument('--dict-pfx', dest='dict_pfx', default='',
@@ -45,6 +50,8 @@ def get_parser():
 
     group = parser.add_argument_group('Model configuration')
     group.add_argument('--model', dest='model', choices=['lstm', 'conv'])
+    group.add_argument('--embedding-size', dest='embedding_size', type=int,
+                       default=200, help="Dimensions of embedding space")
     group.add_argument('--hidden-dim', dest='hidden_dim', default=500, type=int,
                        help="Hidden dimension of the model")
     group.add_argument('--dropout', dest='dropout', default=0.5, type=float,
@@ -60,6 +67,48 @@ def get_parser():
 def main(args=None):
     parser = get_parser()
     args = parser.parse_args(args) if args else parser.parse_args()
+
+    datasets, min_dists, vocab_sizes, nlabels = prep_data(args.datafile,
+                                                          args.preddata,
+                                                          args.test,
+                                                          args.just_eval)
+    train, val, test = datasets
+
+    datakwargs = {'batch_size': args.batch_size, 'vocab_sizes': vocab_sizes}
+    loaders = [
+        build_dataset_iter(train, **datakwargs),
+        build_dataset_iter(val, **datakwargs, is_eval=True),
+        build_dataset_iter(test, **datakwargs, is_eval=True)
+    ]
+
+    emb_sizes = [args.embedding_size,
+                 args.embedding_size // 2,
+                 args.embedding_size // 2]
+    vocab_sizes = [p + 1 for p in vocab_sizes]
+
+    if args.just_eval:
+        raise RuntimeError('No puedo just_eval right now')
+
+    # Building models
+    if args.model == 'lstm':
+        model = RecurrentRgModel(vocab_sizes=vocab_sizes,
+                                 emb_sizes=emb_sizes,
+                                 hidden_dim=args.hidden_dim,
+                                 nlabels=nlabels,
+                                 dropout=args.dropout)
+        model.count_parameters(module_names=['embeddings', 'rnn', 'linear'])
+    else:
+        model = ConvRgModel(vocab_sizes=vocab_sizes,
+                            emb_sizes=emb_sizes,
+                            num_filters=args.num_filters,
+                            hidden_dim=args.hidden_dim,
+                            nlabels=nlabels,
+                            dropout=args.dropout)
+        model.count_parameters(module_names=['embeddings', 'convolutions', 'linear'])
+
+    trainer = Trainer()
+
+    return model, loaders
 
 
 if __name__ == '__main__':
